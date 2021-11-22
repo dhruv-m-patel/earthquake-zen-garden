0b69c6891329facc9b89de84de982a80fd053fa2
Dhruv Patel <dhruv-m-patel@users.noreply.github.com>
2021-11-21 23:00:36 -0600

    feat: Establish site navigation with HeaderNavigation (#3)
    * feat: Add required routes connected to base pages
    
    * refactor: Move site configuration as context value from Page component
    
    * feat: add HeaderNavigation and connect it to home and profile page

---
+++ src/common/components/HeaderNavigation/HeaderNavigation.jsx
@@ -0,0 +1,35 @@
+import classnames from 'classnames/bind';
+import React, { useContext } from 'react';
+import PageContext from '../../context/PageContext';
+import styles from './HeaderNavigation.css';
+
+const cx = classnames.bind(styles);
+
+export default function HeaderNavigation() {
+  const {
+    config: {
+      site: { title, logoImage },
+      profile: { firstName, lastName },
+    },
+  } = useContext(PageContext);
+
+  return (
+    <div className={cx('header')}>
+      <div className={cx('logo')}>
+        <a href="/home" rel="noreferrer noopener">
+          <img src={logoImage} alt="Logo" />
+        </a>
+      </div>
+      <div className={cx('title')}>
+        <h1>
+          <a href="/">{title}</a>
+        </h1>
+      </div>
+      <div className={cx('userDetails')}>
+        <a href="/profile" rel="noreferrer noopener">
+          {firstName} {lastName}
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/common/components/Page/Page.jsx
+++ src/common/components/Page/Page.jsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import DefaultHelmet from '../DefaultHelmet';
 import styles from './Page.css';
+import { DataPropType, ProfilePropType, SitePropType } from '../../prop-types';
+import PageContext from '../../context/PageContext';
+import HeaderNavigation from '../HeaderNavigation';
 
 const cx = classnames.bind(styles);
 
-export default function Page({ title, children }) {
+export default function Page({
+  isFetchingConfiguration,
+  error,
+  config,
+  fetchSiteConfiguration,
+  title,
+  children,
+}) {
+  useEffect(() => {
+    if (!isFetchingConfiguration && !error && !config) {
+      fetchSiteConfiguration();
+    }
+  }, [isFetchingConfiguration, error, config, fetchSiteConfiguration]);
+
   return (
-    <div className={cx('app')}>
-      <DefaultHelmet title={title} />
-      <header className={cx('appHeader')}>
-        <h2>Earthquake Zen Garden</h2>
-      </header>
-      <br />
-      <br />
-      <div className={cx('content')}>{children}</div>
-    </div>
+    <PageContext.Provider value={{ config }}>
+      <div className={cx('app')}>
+        <DefaultHelmet title={title} />
+        {isFetchingConfiguration && <p>Fetching site data...</p>}
+        {!!config && !!Object.keys(config).length && <HeaderNavigation />}
+        <div className={cx('content')}>{children}</div>
+      </div>
+    </PageContext.Provider>
   );
 }
 
 Page.propTypes = {
+  isFetchingConfiguration: PropTypes.bool,
+  error: PropTypes.string,
+  config: {
+    site: SitePropType.isRequired,
+    profile: ProfilePropType.isRequired,
+    data: DataPropType.isRequired,
+  },
+  fetchSiteConfiguration: PropTypes.func,
   children: PropTypes.node.isRequired,
   title: PropTypes.string,
 };
 
 Page.defaultProps = {
+  isFetchingConfiguration: false,
+  error: undefined,
+  config: undefined,
+  fetchSiteConfiguration: () => {},
   title: undefined,
 };
