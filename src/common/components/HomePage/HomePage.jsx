b1615f09c37649b48a42c5d207bf6a43af80eb57
Dhruv Patel <dhruv.codelab@gmail.com>
2021-11-21 22:32:13 -0600

    refactor: Move site configuration as context value from Page component

--- src/common/components/HomePage/HomePage.jsx
+++ src/common/components/HomePage/HomePage.jsx
@@ -1,41 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Page from '../Page';
-import PropTypes from 'prop-types';
-import { DataPropType, ProfilePropType, SitePropType } from '../../prop-types';
 
-export default function HomePage({
-  config,
-  isFetchingConfiguration,
-  error,
-  fetchSiteConfiguration,
-}) {
-  useEffect(() => {
-    if (!isFetchingConfiguration && !error && !config) {
-      fetchSiteConfiguration();
-    }
-  }, [isFetchingConfiguration, error, config, fetchSiteConfiguration]);
-
-  if (!config) {
-    return <Page>Fetching site configuration...</Page>;
-  }
-
+export default function HomePage() {
   return <Page>Home Page</Page>;
 }
 
-HomePage.propTypes = {
-  isFetchingConfiguration: PropTypes.bool,
-  error: PropTypes.string,
-  config: {
-    site: SitePropType.isRequired,
-    profile: ProfilePropType.isRequired,
-    data: DataPropType.isRequired,
-  },
-  fetchSiteConfiguration: PropTypes.func,
-};
+HomePage.propTypes = {};
 
-HomePage.defaultProps = {
-  isFetchingConfiguration: false,
-  error: undefined,
-  config: undefined,
-  fetchSiteConfiguration: () => {},
-};
+HomePage.defaultProps = {};
--- src/common/components/Page/index.js
+++ src/common/components/Page/index.js
@@ -1,1 +1,19 @@
-export { default } from './Page';
+import { connect } from 'react-redux';
+import { fetchConfiguration } from '../../../client/redux/actions';
+import Page from './Page';
+
+function mapStateToProps({ config }) {
+  return {
+    isFetchingConfiguration: config.isFetching,
+    config: config.data,
+    error: config.error,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchSiteConfiguration: () => dispatch(fetchConfiguration()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
